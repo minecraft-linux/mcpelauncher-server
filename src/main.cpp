8fdc5e586f275bf2afe072199a50a9c1f318d227
MCMrARM <mcmrarm@gmail.com>
2018-06-03 21:19:03 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <log.h>
+#include <mcpelauncher/minecraft_utils.h>
+#include <mcpelauncher/crash_handler.h>
+#include <mcpelauncher/path_helper.h>
+#include <minecraft/Common.h>
+#include <mcpelauncher/app_platform.h>
+
+int main(int argc, char *argv[]) {
+    CrashHandler::registerCrashHandler();
+    MinecraftUtils::workaroundLocaleBug();
+    MinecraftUtils::setupForHeadless();
+
+    Log::trace("Launcher", "Loading Minecraft library");
+    void* handle = MinecraftUtils::loadMinecraftLib();
+    Log::info("Launcher", "Loaded Minecraft library");
+
+    Log::debug("Launcher", "Minecraft is at offset 0x%x", MinecraftUtils::getLibraryBase(handle));
+    MinecraftUtils::initSymbolBindings(handle);
+    Log::info("Launcher", "Game version: %s", Common::getGameVersionStringNet().c_str());
+
+    Log::info("Launcher", "Applying patches");
+    // Patches go here
+
+    Log::trace("Launcher", "Initializing AppPlatform (vtable)");
+    LauncherAppPlatform::initVtable(handle);
+    Log::trace("Launcher", "Initializing AppPlatform (create instance)");
+    std::unique_ptr<LauncherAppPlatform> appPlatform (new LauncherAppPlatform());
+    Log::trace("Launcher", "Initializing AppPlatform (initialize call)");
+    appPlatform->initialize();
+
+    //
+
+    MinecraftUtils::workaroundShutdownCrash(handle);
+    return 0;
+}
