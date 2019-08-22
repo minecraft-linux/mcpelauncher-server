e36b10398e34427b8b2805c45723f111bf0d5d96
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2019-08-22 15:27:12 -0400

    Add CPackSettings.cmake (#4)

---
+++ CPackSettings.cmake
@@ -0,0 +1,24 @@
+set(CPACK_PACKAGE_NAME "mcpelauncher-server")
+set(CPACK_PACKAGE_VENDOR "mcpelauncher")
+set(CPACK_PACKAGE_VERSION "${MANIFEST_GIT_COMMIT_HASH}")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "A server for Minecraft: Pocket Edition")
+set(CPACK_PACKAGE_CONTACT "https://github.com/minecraft-linux/mcpelauncher-manifest/issues")
+set(CPACK_GENERATOR "TGZ;DEB")
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE i386)
+set(CPACK_DEBIAN_PACKAGE_VERSION "${BUILD_TIMESTAMP}-${MANIFEST_GIT_COMMIT_HASH}")
+
+set(CPACK_INSTALL_CMAKE_PROJECTS
+        "${CMAKE_BINARY_DIR};mcpelauncher-bin-libs;mcpelauncher-bin-libs;/"
+        "${CMAKE_CURRENT_BINARY_DIR};mcpelauncher-server;mcpelauncher-server;/")
+set(CPACK_OUTPUT_CONFIG_FILE CPackConfig.cmake)
+if (DEB_OS_NAME MATCHES "ubuntu-xenial")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libstdc++6, libcurl3, libx11-6, zlib1g")
+    set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_DEBIAN_PACKAGE_VERSION}~xenial")
+elseif(DEB_OS_NAME MATCHES "ubuntu-bionic")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libstdc++6, libcurl4, libx11-6, zlib1g")
+elseif(DEB_OS_NAME MATCHES "ubuntu-disco")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libstdc++6, libcurl4, libx11-6, zlib1g")
+    set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_DEBIAN_PACKAGE_VERSION}~disco")
+endif()
+
+include(CPack)
